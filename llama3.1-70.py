451cd78c3ab5eccca015a6689121413ac060feb0
Erik <eekihenriksson@gmail.com>
2024-08-05 15:08:53 +0300

    Update llama3.1-70.py

--- llama3.1-70.py
+++ llama3.1-70.py
@@ -6,33 +6,49 @@ os.environ["HF_DATASETS_CACHE"] = ".hf/datasets_cache"
 os.environ["WANDB_DISABLED"] = "true"
 
 import torch
-import transformers
 from dotenv import load_dotenv
 from huggingface_hub import login
+from transformers import AutoModelForCausalLM, AutoTokenizer, AwqConfig
 
 load_dotenv()
 
 login(token=os.getenv("HUGGINGFACE_ACCESS_TOKEN", ""))
 
-model_id = "meta-llama/Meta-Llama-3.1-70B-Instruct"
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
+model_id = "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4"
+quantization_config = AwqConfig(
+    bits=4,
+    fuse_max_seq_len=100000,  # Note: Update this as per your use-case
+    do_fuse=True,
 )
 
-messages = [
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+    device_map="auto",
+    quantization_config=quantization_config,
+)
+
+prompt = [
     {
         "role": "system",
-        "content": "You are a pirate chatbot who always responds in pirate speak!",
+        "content": "You are a helpful assistant, that responds as a pirate.",
     },
-    {"role": "user", "content": "Who are you?"},
+    {"role": "user", "content": "What's Deep Learning?"},
 ]
+inputs = tokenizer.apply_chat_template(
+    prompt,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_tensors="pt",
+    return_dict=True,
+).to("cuda")
 
-outputs = pipeline(
-    messages,
-    max_new_tokens=256,
+outputs = model.generate(**inputs, do_sample=True, max_new_tokens=256)
+print(
+    tokenizer.batch_decode(
+        outputs[:, inputs["input_ids"].shape[1] :], skip_special_tokens=True
+    )[0]
 )
-print(outputs[0]["generated_text"][-1])
