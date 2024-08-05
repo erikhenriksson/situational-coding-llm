50c01a21a6694e1276da8e915b1c1bac4651a3aa
Erik <eekihenriksson@gmail.com>
2024-08-05 23:16:00 +0300

    Update slurmtest.sh

--- slurmtest.sh
+++ slurmtest.sh
@@ -66,6 +66,7 @@ if [[ -z "$SLURM_JOB_ID" ]]; then
   exit $?
 else
   #srun env OLLAMA_MODELS=/scratch/project_2010911/ollama nohup /users/ehenriks/bin/ollama serve > ollama_output.log 2>&1 &
+  export OLLAMA_TMPDIR=/scratch/project_2010911/ollama
   srun env OLLAMA_MODELS=/scratch/project_2010911/ollama /users/ehenriks/bin/ollama serve > ollama_output.log 2>&1 &
   # Actual job script starts here
   source venv/bin/activate
