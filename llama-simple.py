c647d5908e7a8ef73d78ccda8d3c9cbac0955f22
Erik <eekihenriksson@gmail.com>
2024-08-06 15:48:11 +0300

    Update llama-simple.py

--- llama-simple.py
+++ llama-simple.py
@@ -14,6 +14,8 @@ from transformers import BitsAndBytesConfig
 
 load_dotenv()
 
+model_file = "llama-3.1-8b.tsv"
+
 access_token = os.getenv("HUGGINGFACE_ACCESS_TOKEN", "")
 
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
@@ -29,6 +31,9 @@ model = AutoModelForCausalLM.from_pretrained(
 
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
+
+batch_size = 4
+max_text_len = 5000
 
 
 def generate_responses(contexts):
@@ -84,61 +89,6 @@ def generate_responses(contexts):
     return scores_list, explanations_list
 
 
-def generate_response(context):
-    prompt = [
-        {
-            "role": "system",
-            "content": prompts.SYSTEM,
-        },
-        {"role": "user", "content": prompts.MESSAGE.format(context)},
-    ]
-
-    inputs = tokenizer.apply_chat_template(
-        prompt,
-        tokenize=True,
-        add_generation_prompt=True,
-        return_tensors="pt",
-        return_dict=True,
-        attn_implementation="flash_attention_2",
-    ).to("cuda")
-
-    outputs = model.generate(
-        **inputs, do_sample=True, temperature=0.01, max_new_tokens=1024
-    )
-    model_output = tokenizer.batch_decode(
-        outputs[:, inputs["input_ids"].shape[1] :], skip_special_tokens=True
-    )[0]
-
-
-"""
-
-def process_tsv_file(input_file, output_file):
-    with gzip.open(input_file, "rt", encoding="utf-8") as infile, open(
-        output_file.replace(".gz", ""), "w", newline=""
-    ) as outfile:
-        reader = csv.reader(infile, delimiter="\t")
-        writer = csv.writer(outfile, delimiter="\t")
-
-        for row in reader:
-            register, text = row
-            model_output, scores = generate_response(text[:5000])
-
-            # ... code continues
-
-            print(model_output, scores)
-            exit()
-
-            sit_char_str = dict_values_to_string(sit_char)
-            print(register, sit_char_str)
-            writer.writerow([register, sit_char_str])
-            outfile.flush()  # Force write to disk
-
-"""
-
-batch_size = 4
-max_text_len = 5000
-
-
 def process_tsv_file(input_file, output_file):
     def write_batch_results(batch, scores, explanations, writer):
         for i, row in enumerate(batch):
@@ -148,6 +98,14 @@ def process_tsv_file(input_file, output_file):
             print(register, score, explanation, text[:50])
             writer.writerow([register, score, explanation, text])
 
+    proc_rows = 0
+    last_row = []
+    with open(output_file, "r", newline="") as outfile_temp:
+        reader_temp = [row for row in csv.reader(outfile_temp, delimiter="\t") if row]
+        proc_rows = len(reader_temp)
+
+        last_row = reader_temp[-1][-1]
+
     with gzip.open(input_file, "rt", encoding="utf-8") as infile, open(
         output_file.replace(".gz", ""), "w", newline=""
     ) as outfile:
@@ -155,14 +113,27 @@ def process_tsv_file(input_file, output_file):
         writer = csv.writer(outfile, delimiter="\t")
 
         batch = []
+        row_i = 1
+        active = 0
         for row in reader:
-            batch.append(row)
-            if len(batch) == batch_size:
-                texts = [text[:max_text_len] for _, text in batch]
-                scores, explanations = generate_responses(texts)
-                write_batch_results(batch, scores, explanations, writer)
-                batch = []
-                outfile.flush()
+
+            if active:
+                batch.append(row)
+                if len(batch) == batch_size:
+                    texts = [text[:max_text_len] for _, text in batch]
+                    scores, explanations = generate_responses(texts)
+                    write_batch_results(batch, scores, explanations, writer)
+                    batch = []
+                    outfile.flush()
+
+            if row_i == proc_rows:
+                if last_row != row[-1]:
+                    print("Mismatching rows")
+                    exit()
+                else:
+                    active = 1
+
+            row_i += 1
         # Process any remaining rows in the last batch
         if batch:
             texts = [text[:max_text_len] for _, text in batch]
@@ -175,5 +146,7 @@ def process_tsv_file(input_file, output_file):
 input_path = "/scratch/project_2010911/multilingual-CORE"
 io_file = "fi/train.tsv.gz"
 input_file = f"{input_path}/{io_file}"
+output_file = io_file.replace(".tsv.gz", "") + f"_{model_file}"
 
-process_tsv_file(input_file, io_file)
+
+process_tsv_file(input_file, output_file)
