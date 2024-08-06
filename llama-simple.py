1437a118c316084543bb5e3ca0c8abfe77474ef8
Erik <eekihenriksson@gmail.com>
2024-08-06 13:07:34 +0300

    Update llama-simple.py

--- llama-simple.py
+++ llama-simple.py
@@ -27,6 +27,9 @@ model = AutoModelForCausalLM.from_pretrained(
     quantization_config=quantization_config,
 )
 
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
 
 def generate_responses(contexts):
     messages = [
