96fafde6760e1d8895bd7f0e98738001cc0df7f7
Erik <eekihenriksson@gmail.com>
2024-08-13 16:01:17 +0300

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,19 +1,75 @@
 import os
+import gzip
+import sys
+import csv
 
 os.environ["HF_HOME"] = ".hf/hf_home"
 
 from FlagEmbedding import BGEM3FlagModel
 
-model = BGEM3FlagModel(
-    "BAAI/bge-m3", use_fp16=True
-)  # Setting use_fp16 to True speeds up computation with a slight performance degradation
 
-sentences_1 = ["Hello"]
+core_path = os.getenv("CORE_PATH", "")
+language = sys.argv[1]
+core_file = sys.argv[2]
+base_dir = os.path.dirname(os.path.abspath(__file__))
+model_name = "BAAI/bge-m3"
+model_file = model_name.replace("/", "_") + "_embeddings"
 
-embedding = model.encode(
-    sentences_1,
-    batch_size=12,
-    max_length=8192,  # If you don't need such a long length, you can set a smaller value to speed up the encoding process.
-)["dense_vecs"]
+model = BGEM3FlagModel(model_name, use_fp16=True)
 
-print(embedding)
+
+def embed(text):
+
+    embedding = model.encode(
+        [text],
+        batch_size=12,
+        max_length=8192,  # If you don't need such a long length, you can set a smaller value to speed up the encoding process.
+    )["dense_vecs"]
+
+    return " ".join(embedding)
+
+
+def process_tsv_file(input_file):
+    output_file = f"{base_dir}/data/{language}/{model_file}"
+    proc_rows = 0
+    last_row = []
+    try:
+        with open(output_file, "r", newline="") as outfile_temp:
+            reader_temp = [
+                row for row in csv.reader(outfile_temp, delimiter="\t") if row
+            ]
+            proc_rows = len(reader_temp)
+
+            last_row = reader_temp[-1][-1]
+    except:
+        pass
+    print(proc_rows)
+    with gzip.open(input_file, "rt", encoding="utf-8") as infile, open(
+        output_file, "a", newline=""
+    ) as outfile:
+        reader = csv.reader(infile, delimiter="\t")
+        writer = csv.writer(outfile, delimiter="\t")
+        row_i = 1
+        active = 0
+        if not proc_rows:
+            active = 1
+        for row in reader:
+            if active:
+                register, text = row
+                scores, explanations = embed(text[:5000])
+                writer.writerow([register, scores, explanations, text])
+                outfile.flush()  # Force write to disk
+
+            if row_i == proc_rows:
+                if last_row != row[-1]:
+                    print("Mismatching rows")
+                    exit()
+                else:
+                    active = 1
+
+            row_i += 1
+
+
+input_file = f"{core_path}/{language}/{core_file}"
+
+process_tsv_file(input_file)
