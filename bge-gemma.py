39f4c2d1c46e50fa6af67f149c2ef1f71bc6b375
Erik <eekihenriksson@gmail.com>
2024-08-15 12:35:08 +0300

    Update bge-gemma.py

--- bge-gemma.py
+++ bge-gemma.py
@@ -2,48 +2,61 @@ import torch
 import torch.nn.functional as F
 
 from torch import Tensor
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, BitsAndBytesConfig
 
 
-def last_token_pool(last_hidden_states: Tensor,
-                 attention_mask: Tensor) -> Tensor:
-    left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
+def last_token_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+    left_padding = attention_mask[:, -1].sum() == attention_mask.shape[0]
     if left_padding:
         return last_hidden_states[:, -1]
     else:
         sequence_lengths = attention_mask.sum(dim=1) - 1
         batch_size = last_hidden_states.shape[0]
-        return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
+        return last_hidden_states[
+            torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths
+        ]
 
 
 def get_detailed_instruct(task_description: str, query: str) -> str:
-    return f'<instruct>{task_description}\n<query>{query}'
+    return f"<instruct>{task_description}\n<query>{query}"
 
 
-task = 'Given a web search query, retrieve relevant passages that answer the query.'
+task = "Given a web search query, retrieve relevant passages that answer the query."
 queries = [
-    get_detailed_instruct(task, 'how much protein should a female eat'),
-    get_detailed_instruct(task, 'summit define')
+    get_detailed_instruct(task, "how much protein should a female eat"),
+    get_detailed_instruct(task, "summit define"),
 ]
 # No need to add instructions for documents
 documents = [
     "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
-    "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments."
+    "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments.",
 ]
 input_texts = queries + documents
 
-tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-multilingual-gemma2')
-model = AutoModel.from_pretrained('BAAI/bge-multilingual-gemma2')
+tokenizer = AutoTokenizer.from_pretrained("BAAI/bge-multilingual-gemma2")
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+model = AutoModel.from_pretrained(
+    "BAAI/bge-multilingual-gemma2", quantization_config=quantization_config
+)
 model.eval()
 
 max_length = 4096
 # Tokenize the input texts
-batch_dict = tokenizer(input_texts, max_length=max_length, padding=True, truncation=True, return_tensors='pt', pad_to_multiple_of=8)
+batch_dict = tokenizer(
+    input_texts,
+    max_length=max_length,
+    padding=True,
+    truncation=True,
+    return_tensors="pt",
+    pad_to_multiple_of=8,
+)
 
 with torch.no_grad():
     outputs = model(**batch_dict)
-    embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-    
+    embeddings = last_token_pool(
+        outputs.last_hidden_state, batch_dict["attention_mask"]
+    )
+
 # normalize embeddings
 embeddings = F.normalize(embeddings, p=2, dim=1)
 scores = (embeddings[:2] @ embeddings[2:].T) * 100
